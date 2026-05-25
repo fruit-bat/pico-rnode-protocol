5aa6a67e89cc5fe3acd003bf0e9f5f850c7e41f2
fruit-bat <pmscull@gmail.com>
2026-05-25 22:34:42 +0100

    wip

---
+++ tests/test_command_encoder.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 fruit-bat
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "pico-rnode-protocol-command-encoder.h"
+#include "test_utils.h"
+
